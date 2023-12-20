dcd65d343376edb82e33d671981843cbe582e852
Sdevanahally <154476812+Sdevanahally@users.noreply.github.com>
2023-12-20 18:14:26 -0500

    Update Lab 0

--- Lab 0- Color Conversion.py
+++ Lab 0- Color Conversion.py
@@ -12,9 +12,3 @@ y=(w-(b_input/255))/w
 k=1-w
 cmyk_output = (c+m+y+k)
 print(cmyk_output)
-
-
-#so basically first you take the RGB input and then
-#then you solve for w using the formula w= max((r_input/255), (g_input/255), (b_input/255))
-#then you put it into the formula for cmyk -- c= (((w-(r/255))/w), m= (((w-(g/255))/w), y= (((w-(b/255))/w), k= (1-w)
-#return CYMK
